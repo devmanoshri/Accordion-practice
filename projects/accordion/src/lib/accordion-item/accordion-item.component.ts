6f8267a9da0cab856a14d97873c42e886706a747
devmanoshri <manoshri.no@gmail.com>
2024-10-20 22:04:45 +0200

    create accordion library

---
+++ projects/accordion/src/lib/accordion-item/accordion-item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-accordion-item',
+  templateUrl: './accordion-item.component.html',
+  styleUrl: './accordion-item.component.css'
+})
+export class AccordionItemComponent {
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AccordionModule } from "../../projects/accordion/src/lib/accordion.module";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AccordionModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
